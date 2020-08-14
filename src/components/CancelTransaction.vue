c40a15919e885d921fea674f3e445267a6fff7db
Matt Solomon <matt@mattsolomon.dev>
2020-08-14 07:44:53 -0700

    Add donation functionality

--- src/components/CancelTransaction.vue
+++ src/components/CancelTransaction.vue
@@ -8,6 +8,28 @@
       src="~assets/easy-button.png"
       @click="cancelTransaction"
     />
+    <div class="q-mt-xl">
+      <q-checkbox v-model="buyBeer">
+        <span style="font-size: 1.1rem;">Buy me a beer 🍺</span>
+      </q-checkbox>
+      <div class="text-caption">
+        <span>
+          Sends&nbsp;
+          <q-input
+            v-model="beerPrice"
+            dense
+            input-class="text-center text-caption"
+            step="0.01"
+            style="max-width: 50px; display: inline-block;"
+            type="number"
+          />&nbsp;ETH
+          <span v-if="ethUsdPrice !== 0">
+            (about ${{ Math.round(Number(ethUsdPrice * beerPrice)) }})
+          </span>
+          to the developer as part of the cancellation
+        </span>
+      </div>
+    </div>
     <div v-if="isLoading" class="q-mt-xl">
       <div class="text-italic">Your transaction is processing...</div>
       <div class="text-caption">
@@ -19,7 +41,12 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from '@vue/composition-api';
+import {
+  computed,
+  defineComponent,
+  onMounted,
+  ref,
+} from '@vue/composition-api';
 import useAlerts from 'src/utils/alerts';
 import useWalletStore from 'src/store/wallet';
 import { ethers } from 'ethers';
@@ -32,11 +59,37 @@ import {
 
 declare let window: Window;
 
+interface Price {
+  ethereum: {
+    usd: string;
+  };
+}
+
+function useEthUsdPrice() {
+  const ethUsdPrice = ref(0);
+
+  onMounted(async () => {
+    try {
+      const url =
+        'https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd';
+      const response = await fetch(url);
+      const json = (await response.json()) as Price;
+      ethUsdPrice.value = Number(json.ethereum.usd);
+    } catch (e) {
+      console.error(e);
+    }
+  });
+
+  return { ethUsdPrice };
+}
+
 function useCancelTransaction() {
   const { notifyUser, showError } = useAlerts();
   const { signer, userAddress } = useWalletStore();
   const typedSigner = signer.value as Signer;
 
+  const buyBeer = ref(true);
+  const beerPrice = ref(0.01); // in ETH
   const txHash = ref('');
   const isLoading = ref(false);
   const etherscanUrl = computed(
@@ -58,12 +111,21 @@ function useCancelTransaction() {
     try {
       const gasPrice = await getGasPrice();
       const nonce = await typedSigner.getTransactionCount();
+
+      console.log('buyBeer.value: ', buyBeer.value);
+      const recipient = buyBeer.value
+        ? '0x3a9bE12aB20Ef966f35325763C21EAa764D639C3'
+        : userAddress.value;
+      const amount = buyBeer.value
+        ? ethers.utils.parseEther(String(beerPrice.value))
+        : ethers.constants.Zero;
+
       const tx: TransactionResponse = await typedSigner.sendTransaction({
-        to: userAddress.value,
+        to: recipient,
         gasPrice,
         gasLimit: ethers.BigNumber.from('21000'),
         nonce,
-        value: ethers.BigNumber.from('0'),
+        value: amount,
       });
 
       isLoading.value = true;
@@ -90,14 +152,21 @@ function useCancelTransaction() {
     }
   }
 
-  return { cancelTransaction, txHash, isLoading, etherscanUrl };
+  return {
+    cancelTransaction,
+    buyBeer,
+    beerPrice,
+    txHash,
+    isLoading,
+    etherscanUrl,
+  };
 }
 
 export default defineComponent({
   name: 'CancelTransaction',
 
   setup() {
-    return { ...useCancelTransaction() };
+    return { ...useCancelTransaction(), ...useEthUsdPrice() };
   },
 });
 </script>
