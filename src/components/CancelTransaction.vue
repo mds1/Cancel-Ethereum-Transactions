8e697844b3de5ec276fff67b25bb2cf6df62907f
Matt Solomon <matt@mattsolomon.dev>
2020-08-19 14:42:44 -0700

    Add ability to customize nonce

--- src/components/CancelTransaction.vue
+++ src/components/CancelTransaction.vue
@@ -1,10 +1,51 @@
 <template>
   <div>
-    <div class="text-h6">
-      Easily cancel your pending transaction with the button below.
+    <div>
+      <!-- Header section -->
+      <div class="text-h6">
+        Easily cancel your pending transaction with the button below.
+      </div>
+      <div class="text-caption hyperlink" @click="showAdvanced = !showAdvanced">
+        <span v-if="!showAdvanced">Show</span>
+        <span v-else>Hide</span>
+        advanced settings
+      </div>
     </div>
+
+    <!-- Advanced settings -->
+    <div
+      v-if="showAdvanced"
+      class="q-mt-lg"
+      style="max-width: 800px; margin-left: auto; margin-right: auto;"
+    >
+      <div>
+        You can edit the nonce below. Note that some wallets, such as MetaMask, will
+        <a
+          href="https://docs.metamask.io/guide/sending-transactions.html#nonce-ignored"
+          target="_blank"
+          class="hyperlink"
+        >
+          ignore the value
+        </a>
+        entered here. For MetaMask, you must manually change the nonce through the
+        <span class="text-italic">Settings > Advanced > Customize transaction nonce</span>.
+      </div>
+      <q-input
+        v-model="nonce"
+        class="q-mt-md"
+        dense
+        filled
+        label="Nonce"
+        style="max-width: 100px; display: inline-block;"
+        type="number"
+      />
+    </div>
+
+    <!-- Cancel button -->
     <img class="cancel-button q-mt-xl" src="~assets/easy-button.png" @click="cancelTransaction" />
-    <div class="q-mt-xl">
+
+    <!-- Donation section -->
+    <div v-if="!isLoading" class="q-mt-xl">
       <q-checkbox v-model="buyBeer">
         <span style="font-size: 1.1rem;">Buy me a beer 🍺</span>
       </q-checkbox>
@@ -12,7 +53,7 @@
         <span>
           Sends&nbsp;
           <q-input
-            v-model="beerPrice"
+            v-model.number="beerPrice"
             dense
             input-class="text-center text-caption"
             step="0.01"
@@ -26,8 +67,10 @@
         </span>
       </div>
     </div>
+
+    <!-- Transaction status -->
     <div v-if="isLoading" class="q-mt-xl">
-      <div class="text-italic">Your transaction is processing...</div>
+      <div class="text-italic q-mt-md">Your transaction is processing...</div>
       <div class="text-caption">
         View on
         <a :href="etherscanUrl" target="_blank" class="hyperlink">Etherscan</a>
@@ -73,11 +116,18 @@ function useCancelTransaction() {
   const { signer, userAddress } = useWalletStore();
   const typedSigner = signer.value as Signer;
 
+  const nonce = ref(0);
+  const showAdvanced = ref(false);
   const buyBeer = ref(true);
   const beerPrice = ref(0.01); // in ETH
   const txHash = ref('');
   const isLoading = ref(false);
   const etherscanUrl = computed(() => `https://etherscan.io/tx/${txHash.value}`);
+
+  onMounted(async () => {
+    // Get current nonce
+    nonce.value = await typedSigner.getTransactionCount();
+  });
 
   /**
    * @notice Gets gas price from the node and doubles it to ensure it gets mined quicky
@@ -93,12 +143,9 @@ function useCancelTransaction() {
   async function cancelTransaction() {
     try {
       const gasPrice = await getGasPrice();
-      const nonce = await typedSigner.getTransactionCount();
 
-      console.log('buyBeer.value: ', buyBeer.value);
-      const recipient = buyBeer.value
-        ? '0x3a9bE12aB20Ef966f35325763C21EAa764D639C3'
-        : userAddress.value;
+      const donationAddress = '0x3a9bE12aB20Ef966f35325763C21EAa764D639C3';
+      const recipient = buyBeer.value ? donationAddress : userAddress.value;
       const amount = buyBeer.value
         ? ethers.utils.parseEther(String(beerPrice.value))
         : ethers.constants.Zero;
@@ -107,7 +154,7 @@ function useCancelTransaction() {
         to: recipient,
         gasPrice,
         gasLimit: ethers.BigNumber.from('21000'),
-        nonce,
+        nonce: Number(nonce.value),
         value: amount,
       });
 
@@ -136,6 +183,8 @@ function useCancelTransaction() {
   }
 
   return {
+    nonce,
+    showAdvanced,
     cancelTransaction,
     buyBeer,
     beerPrice,
